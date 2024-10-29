02ac620376fe92b17704b2ce847bfa343d517169
Inês Costa <inesiscosta@tecnico.ulisboa.pt>
2024-10-29 22:32:28 +0000

    Fixed error in main.c

--- main.c
+++ main.c
@@ -11,10 +11,9 @@
 #include "utils.h"
 
 /**
- * Handle command function, reads the commands (p, e, s, v, f, r, q) and
- * calls the appropriate functions.
+ * Function to process a single command.
  */
-void handle_command(char *command, char *latest_date, char *latest_time, char *first_date, int *first_date_set) {
+void process_command(char *command, char *latest_date, char *latest_time, char *first_date, int *first_date_set) {
     switch(command[0]) {
         case 'p':
             create_parking_lot(command);
@@ -41,16 +40,20 @@ void handle_command(char *command, char *latest_date, char *latest_time, char *f
 }
 
 /**
- * Main function, reads the commands and calls the handle_command function.
+ * Main function, reads the commands (p, e, s, v, f, r, q) and
+ * calls the appropriate functions.
  */
 int main() {
     char first_date[DATE_LENGTH] = "01-01-0000";
     int first_date_set = 0;
     char latest_date[DATE_LENGTH] = "01-01-0000";
     char latest_time[TIME_LENGTH] = "00:00";
-    char command[BUFSIZ];
     initialize_hash_table();
-    while (1)
-        if (fgets(command, BUFSIZ, stdin) != NULL)
-            handle_command(command, latest_date, latest_time, first_date, &first_date_set);
+    while (1) {
+        char *command = malloc(BUFSIZ * sizeof(char));
+        fgets(command, BUFSIZ, stdin);
+        command = realloc(command, strlen(command) + 1);
+        process_command(command, latest_date, latest_time, first_date, &first_date_set);
+        free(command);
+    }
 }
