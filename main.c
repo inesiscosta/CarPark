835f61d20b42919b5f1b50bedda3b1dadebed1d0
Inês Costa <inesiscosta@tecnico.ulisboa.pt>
2024-10-29 22:05:13 +0000

    Fixed main function nloc warning >30

--- main.c
+++ main.c
@@ -11,43 +11,46 @@
 #include "utils.h"
 
 /**
- * Main function, reads the commands (p, e, s, v, f, r, q) and
+ * Handle command function, reads the commands (p, e, s, v, f, r, q) and
  * calls the appropriate functions.
+ */
+void handle_command(char *command, char *latest_date, char *latest_time, char *first_date, int *first_date_set) {
+    switch(command[0]) {
+        case 'p':
+            create_parking_lot(command);
+            break;
+        case 'e':
+            register_vehicle_entry(command, latest_date, latest_time, first_date, (char *)first_date_set);
+            break;
+        case 's':
+            register_vehicle_exit(command, latest_date, latest_time);
+            break;
+        case 'v':
+            list_vehicle_entries_and_exits(command);
+            break;
+        case 'f':
+            list_billings(command, latest_date, latest_time, first_date);
+            break;
+        case 'r':
+            remove_parking_lot(command);
+            break;
+        case 'q':
+            cleanup(command);
+            exit(0);
+    }
+}
+
+/**
+ * Main function, reads the commands and calls the handle_command function.
  */
 int main() {
     char first_date[DATE_LENGTH] = "01-01-0000";
     int first_date_set = 0;
     char latest_date[DATE_LENGTH] = "01-01-0000";
     char latest_time[TIME_LENGTH] = "00:00";
+    char command[BUFSIZ];
     initialize_hash_table();
-    while (1) {
-        char *command = malloc(BUFSIZ * sizeof(char));
-        fgets(command, BUFSIZ, stdin);
-        command = realloc(command, strlen(command) + 1);
-        switch(command[0]) {
-            case 'p':
-                create_parking_lot(command);
-                break;
-            case 'e':
-                register_vehicle_entry(command, latest_date, latest_time,
-                first_date, (char *)&first_date_set);
-                break;
-            case 's':
-                register_vehicle_exit(command, latest_date, latest_time);
-                break;
-            case 'v':
-                list_vehicle_entries_and_exits(command);
-                break;
-            case 'f':
-                list_billings(command, latest_date, latest_time, first_date);
-                break;
-            case 'r':
-                remove_parking_lot(command);
-                break;
-            case 'q':
-                cleanup(command);
-                return 0;
-        }
-        free(command);
-    }
+    while (1)
+        if (fgets(command, BUFSIZ, stdin) != NULL)
+            handle_command(command, latest_date, latest_time, first_date, &first_date_set);
 }
